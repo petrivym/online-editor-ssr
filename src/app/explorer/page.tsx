731f0dafc95839feb695cf682584851c13417187
YaroslavPetrivv <yaroslav.petriv@softblues.io>
2023-10-13 13:03:56 +0300

    init app

---
+++ src/app/explorer/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Explorer() {
+  return (
+    <div> 
+      <h1>Explorer</h1>
+        <Link href={"/page-mdx"}>page-md</Link>
+    </div>
+  )
+};
---
+++ src/app/page-mdx/title.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SecondTitle = () => {
+  return (
+    <div>
+      <h2>React Component</h2>
+    </div>
+  )
+};
+
+export default SecondTitle;
